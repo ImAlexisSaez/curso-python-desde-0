e1c6cb5a047bdd3eb04060b70934ffbc6ddf9d7b
Alexis Sáez <cucoalexis@hotmail.com>
2019-05-01 22:18:30 +0200

    Añade archivos correspondientes a la lección 14

---
+++ lecciones/prac14_bucles1.py
@@ -0,0 +1,14 @@
+for i in [1, 2, 3]:
+	print("Hola")
+
+for i in ["enero", "febrero", "marzo"]:
+	print("Hola")
+
+for i in [1, 2, 3]:
+	print(i)
+
+for i in ["enero", "febrero", "marzo"]:
+	print(i)
+
+for meses in ["enero", "febrero", "marzo"]:
+	print(meses)
