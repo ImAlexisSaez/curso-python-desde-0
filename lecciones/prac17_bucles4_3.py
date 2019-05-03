8cf481b4587cba18f8d71e588733cbc0782e63a7
Alexis Sáez <cucoalexis@hotmail.com>
2019-05-03 12:36:14 +0200

    Añade archivos correspondientes a la lección 17

---
+++ lecciones/prac17_bucles4_3.py
@@ -0,0 +1,21 @@
+import math
+
+print("Programa de cálculo de raíces cuadradas")
+
+numero = int(input("Introduce un número: "))
+
+intentos = 0 # Para ejecutar el bucle while un número de veces determinado
+
+while numero < 0:
+	print("No se puede hallar la raíz de un número negativo.")
+	if intentos == 2:
+		print("Has consumido demasiados intentos.")
+		print("El programa ha finalizado")
+		break
+	numero = int(input("Introduce un número: "))
+	if numero < 0:
+		intentos += 1
+
+if intentos < 2:
+	solucion = math.sqrt(numero)
+	print(f"La raíz cuadrada de {numero} es {solucion}.")
---
+++ lecciones/prac17_ejer2.py
@@ -0,0 +1,13 @@
+print("Cálculo de la suma de una serie de números positivos.")
+print("Instrucciones: ")
+print("- Introduce tantos números positivos como desees sumar.")
+print("- Introduce un número negativo para calcular la suma.")
+
+suma = 0
+num  = int(input("Introduce un número positivo: "))
+
+while num > 0:
+	suma += num
+	num  = int(input("Introduce un número positivo: "))
+
+print(f"La suma de los valores positivos introducios es {suma}.")
