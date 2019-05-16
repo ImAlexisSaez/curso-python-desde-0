85df8dddddca4bf62bf152bba504487d64fae5d0
Alexis Sáez <cucoalexis@hotmail.com>
2019-05-16 09:20:17 +0200

    Añade los archivos correspondientes a la lección 40

---
+++ lecciones/prac40_serializacion2_2.py
@@ -0,0 +1,34 @@
+import pickle
+
+
+class Vehiculo():
+    def __init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+        self.enmarcha = False
+        self.acelera = False
+        self.frena = False
+
+    def arrancar(self):
+        self.enmarcha = True
+
+    def acelerar(self):
+        self.acelera = True
+
+    def frenar(self):
+        self.frena = True
+
+    def estado(self):
+        print("Marca:", self.marca, "\nModelo:", self.modelo, "\nEn marcha:",
+              self.enmarcha, "\nAcelerando:", self.acelera, "\nFrenando:",
+              self.frena)
+
+
+fichero = open("prac40_files/coches", "rb")
+
+coches = pickle.load(fichero)
+
+print(coches)
+
+for coche in coches:
+    print(coche.estado())
