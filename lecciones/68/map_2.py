50d3edbac52b704b6c7c4c1276671f80380ba683
Alexis Sáez <cucoalexis@hotmail.com>
2019-06-09 10:19:26 +0200

    Añade los archivos correspondientes a la lección 68

---
+++ lecciones/68/map_2.py
@@ -0,0 +1,28 @@
+class Empleado:
+    def __init__(self, nombre, cargo, salario):
+        self.nombre = nombre
+        self.cargo = cargo
+        self.salario = salario
+
+    def __str__(self):
+        return f"{self.nombre} trabaja como {self.cargo} y cobra {self.salario} €."
+
+
+lista_empleados = [
+    Empleado("Juan", "Director", 6700),
+    Empleado("Ana", "Presidenta", 7500),
+    Empleado("Antonio", "Administrativo", 1200),
+    Empleado("Sara", "Secretaria", 1250),
+    Empleado("Mario", "Botones", 1000)
+]
+
+
+def calcula_comision(empleado):
+    if empleado.salario <= 3000:
+        empleado.salario *= 1.03
+    return empleado
+
+
+lista_empleados_comision = map(calcula_comision, lista_empleados)
+
+[print(e) for e in lista_empleados_comision]
